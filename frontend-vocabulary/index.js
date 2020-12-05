cce5d26680c76ec35491078b435d676d2cdba860
devWRM <mcwen@comcast.net>
2020-12-05 17:19:00 -0500

    built the fetchStudents function, created a const BASE_URL, and added fetchStudents() call to the DOMContentLoaded event listener

--- frontend-vocabulary/index.js
+++ frontend-vocabulary/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello!")
+    fetchStudents()
+
+
 })
+
+const BASE_URL = "http://localhost:3000"
+
+
+// CRUD Read
+function fetchStudents() {
+    fetch(`${BASE_URL}/students`)
+    .then(resp => resp.json())
+    .then(students => {
+        for(const student of students) {
+            let s = new Student(student.id, student.name, student.nickname, student.email)
+            s.renderStudent();
+        }
+    })
+
+}
+
+
+
+
+// CRUD Create
+
+
+
+
+
+// CRUD Delete
+
