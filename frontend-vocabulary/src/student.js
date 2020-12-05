49152fb2650bce06d9461a3f0f8d57df604048de
devWRM <mcwen@comcast.net>
2020-12-05 14:40:04 -0500

    created source folder and added student & word JS files, added a Student & Word class with constructor in each

---
+++ frontend-vocabulary/src/student.js
@@ -0,0 +1,8 @@
+class Student {
+    constructor(id, name, nickname, email) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+    }
+}
---
+++ frontend-vocabulary/src/word.js
@@ -0,0 +1,5 @@
+class Word {
+    constructor(spelling, pos, definition, sentence, student_id) {
+        
+    }
+}
