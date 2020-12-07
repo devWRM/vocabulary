5fd57c72ec2f8cabbab9021e9067551ce19fb7f3
devWRM <mcwen@comcast.net>
2020-12-07 06:32:55 -0500

    created wordFormInput object for fetch Post request

--- frontend-vocabulary/index.js
+++ frontend-vocabulary/index.js
@@ -88,11 +88,94 @@ function deleteStudent() {
 
 
 // CRUD Create WORD
-function addWordForm() {
-    
+
+function showForm() {
+    let hideStudents = document.getElementById("container")
+    hideStudents.style.display = "none";
+
+    let studentId = parseInt(event.target.dataset.id)
+    addWordForm(studentId)
+}
+
+function addWordForm(id) {
+    // let studentId = parseInt(event.target.dataset.id)
+
+    let formDiv = document.getElementById("word-form")
+
+    formDiv.innerHTML +=
+        `
+        <form id = "this-word-form"> 
+        Enter a vocabulary word for ${id}:</br>
+            <input type="string" id="spelling" placeholder="spell the word"></input></br>
+            <input type="text" id="pos" placeholder="part of speech"></input></br>
+            <input type="text" id="definition" placeholder="define word"></input></br>
+            <input type="text" id="sentence" placeholder="use word in a sentence"></input></br>
+            <input type="submit" value="new word">   
+        </form>   
+        `
+
+    let thisWordForm = document.getElementById("this-word-form")
+    thisWordForm.addEventListener("submit", wordFormSubmission(id))
 
 }
 
+function wordFormSubmission(id) {
+    event.preventDefault()
+    
+    
+
+    // INPORTANT: Remember to add .value to get data out of form <input> field
+    let spelling = document.getElementById("spelling").value
+    let pos = document.getElementById("pos").value
+    let definition = document.getElementById("definition").value
+    let sentence = document.getElementById("sentence").value
+    
+
+    let wordFormInput = {
+        spelling: spelling,
+        pos: pos,
+        definition: definition,
+        sentence: sentence,
+        student_id: id
+    }
+    
+    fetch(`${BASE_URL}/words`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify(wordFormInput)
+    })
+    .then(resp => resp.json())
+    .then(apiWord => console.log(apiWord))
+
+
+
+    // .then(apiStudent => {
+        
+    //     let student = new Student(apiStudent.id, apiStudent.name, apiStudent.nickname, apiStudent.email)
+    //     student.renderStudent();
+    // })
+
+
+
+
+
+
+
+
+
+
+
+
+    // let deleteWordForm = document.getElementById("word-form")
+    // deleteWordForm.innerHTML = ""
+
+    // let hideStudents = document.getElementById("container")
+    // hideStudents.style.display = "none";
+
+}
 
 
 
