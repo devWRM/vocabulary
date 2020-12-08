8f6adbba412aa745a6eecd0c2e3cb0d9bbd69211
devWRM <mcwen@comcast.net>
2020-12-08 12:49:08 -0500

    adding a renderStudentWord method in addition to the render Word method

--- frontend-vocabulary/src/word.js
+++ frontend-vocabulary/src/word.js
@@ -6,7 +6,44 @@ class Word {
         this.sentence = sentence;
     }
 
-    
+
+
+    renderStudentWord() {
+        let wordsContainer = document.getElementById("words-container")
+        wordsContainer.innerHTML +=
+
+        `
+        <p>
+            <b>Vocabulary Word:</b> ${this.spelling}</br>           
+            <b>Part of speech:</b> ${this.pos}</br>
+            <b>Definition:</b> ${this.definition}</br>
+            <b>Sentence:</b> ${this.sentence}
+             
+        </p>
+        `
+    }
+
+
+
+
+
+    renderWord() {
+        let wordsContainer = document.getElementById("words-container")
+        wordsContainer.innerHTML +=
+
+        `
+        <p>
+            <b>Vocabulary Word:</b> ${this.spelling}</br>           
+            <b>Part of speech:</b> ${this.pos}</br>
+            <b>Definition:</b> ${this.definition}</br>
+            <b>Sentence:</b> ${this.sentence}
+             
+        </p>
+        `
+    }
+
+
+
 
 
 
