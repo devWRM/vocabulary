4ec31135a3347ac581945cc4696cf31a98e7757f
devWRM <mcwen@comcast.net>
2020-12-08 12:44:38 -0500

    added different ways to reference deleting its children when a parent is deleted

--- backend-vocabulary/app/models/student.rb
+++ backend-vocabulary/app/models/student.rb
@@ -1,5 +1,8 @@
 class Student < ApplicationRecord
     has_many :words, :dependent => :destroy
 
-    # has_many :words, :dependent => :delete_all  
+    # has_many :words, :dependent => :delete_all 
+
+    # dependent::destroy 
+    # dependent::delete_all
 end
