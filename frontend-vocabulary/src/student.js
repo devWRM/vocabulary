b1690ef9a6acfd844db7a798957edaa96bb3c35b
devWRM <mcwen@comcast.net>
2020-12-09 07:15:47 -0500

    added <div> lines to divide page content

--- frontend-vocabulary/src/student.js
+++ frontend-vocabulary/src/student.js
@@ -14,8 +14,8 @@ class Student {
         studentsContainer.innerHTML +=
         `
         <ul>
-            <div>===========================</div>
-            <h4>STUDENT: ${this.nickname}</h4>
+        <div>___________________________</div>
+        <h4>STUDENT: ${this.nickname}</h4>
             <li>${this.name} <b>::</b> ${this.email}</li> 
 
             <button class="wordlist" data-id=${this.id} onclick="fetchSingleWordList()">${this.nickname}'s words</button> 
@@ -23,6 +23,7 @@ class Student {
             <button class="add-word" data-id=${this.id} onclick="showForm()">add a word for ${this.nickname}</button> 
 
             <button class="delete-button" data-id=${this.id} onclick="deleteStudent()">delete ${this.nickname}</button> 
+            <div>===========================</div>
 
         </ul>
         `
