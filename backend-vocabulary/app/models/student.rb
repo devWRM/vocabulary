13a49ffaac6f94834382ac42f899e6d151aa0e80
devWRM <mcwen@comcast.net>
2020-12-10 08:44:46 -0500

    added validation for all fields but it's NOT working

--- backend-vocabulary/app/models/student.rb
+++ backend-vocabulary/app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
     has_many :words, :dependent => :destroy
 
+    validates :name, :nickname, :email, presence: true
+
     # has_many :words, :dependent => :delete_all 
 
     # dependent::destroy 
