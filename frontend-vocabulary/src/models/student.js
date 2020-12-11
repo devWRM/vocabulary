14f727b03e90f592108083e4d32df4b63ebd2a20
devWRM <mcwen@comcast.net>
2020-12-11 07:12:36 -0500

    in renderStudent function enclosed the students-container in <div> tag with a data-id set to this.id for this student's id

--- frontend-vocabulary/src/models/student.js
+++ frontend-vocabulary/src/models/student.js
@@ -13,19 +13,21 @@ class Student {
         let studentsContainer = document.getElementById("students-container")
         studentsContainer.innerHTML +=
         `
-        <ul>
-        <div>___________________________</div>
-        <h4>STUDENT: ${this.nickname}</h4>
-            <li>${this.name} <b>::</b> ${this.email}</li> 
+        <div data-id=${this.id}>
+            <ul>
+            <div>___________________________</div>
+            <h4>STUDENT: ${this.nickname}</h4>
+                <li>${this.name} <b>::</b> ${this.email}</li> 
 
-            <button class="wordlist" data-id=${this.id} onclick="fetchSingleWordList()">${this.nickname}'s words</button> 
+                <button class="wordlist" data-id=${this.id} onclick="fetchSingleWordList()">${this.nickname}'s words</button> 
 
-            <button class="add-word" data-id=${this.id} onclick="showForm()">add a word for ${this.nickname}</button> 
+                <button class="add-word" data-id=${this.id} onclick="showForm()">add a word for ${this.nickname}</button> 
 
-            <button class="delete-button" data-id=${this.id} onclick="deleteStudent()">delete ${this.nickname}</button> 
-            <div>===========================</div>
+                <button class="delete-button" data-id=${this.id} onclick="deleteStudent()">delete ${this.nickname}</button> 
+                <div>===========================</div>
 
-        </ul>
+            </ul>
+        </div>
         `
 
         
