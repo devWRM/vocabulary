c6a238ff8502d1bec2791af0946cb5253d402ae7
devWRM <mcwen@comcast.net>
2020-12-21 16:51:13 -0500

    added the delete action to destroy ALL words

--- backend-vocabulary/app/controllers/words_controller.rb
+++ backend-vocabulary/app/controllers/words_controller.rb
@@ -48,6 +48,11 @@ class WordsController < ApplicationController
     @word.destroy
   end
 
+  # DELETE /words
+  def delete
+    Word.destroy_all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_word
